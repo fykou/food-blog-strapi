a69075a02c667baefb34afcbceb3bb59335c44a9
hoyby <alex.hoeyby@gmail.com>
2022-08-31 11:09:27 +0200

    Switch to AWS

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,14 +1,12 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      host: env("DATABASE_HOST", "localhost"),
+      port: env("DATABASE_PORT", "5432"),
+      database: env("DATABASE_NAME", "graphql"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "postgres"),
       ssl: {
         rejectUnauthorized: false,
       },
