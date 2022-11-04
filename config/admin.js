caeae4b513b0473050f3f64c227cfbd1b516cb39
hoyby <alex.hoeyby@gmail.com>
2022-11-04 15:29:16 +0100

    Bump versions, cleanup enviroment vars, fix image not showing

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  apiToken: { salt: env("API_TOKEN_SALT") },
-  auth: {
-    secret: env("ADMIN_JWT_SECRET", "c8ad0004e7f2be21a1985e5a411e38e3"),
-  },
+	apiToken: { salt: env("API_TOKEN_SALT") },
+	auth: {
+		secret: env("ADMIN_JWT_SECRET"),
+	},
 });
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env("DATABASE_PORT", "5432"),
-      database: env("DATABASE_NAME", "graphql"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "postgres"),
+      host: env("DATABASE_HOST"),
+      port: env("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
       ssl: {
         rejectUnauthorized: false,
       },
